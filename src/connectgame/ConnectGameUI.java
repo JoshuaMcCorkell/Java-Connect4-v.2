d731d8fba6ccfd4439d2d90a113960ac0b0446d7
Joshua McCorkell <joshua@themccorkells.com>
2022-03-19 12:32:36 +1000

    Adding game board and player turn handing

--- src/connectgame/ConnectGameUI.java
+++ src/connectgame/ConnectGameUI.java
@@ -4,6 +4,9 @@ import connectgame.engine.*;
 
 import java.awt.event.MouseEvent;
 
+import javax.naming.directory.InvalidAttributeValueException;
+import javax.naming.directory.InvalidAttributesException;
+
 public class ConnectGameUI {
 
     public enum GameMode {
@@ -15,26 +18,51 @@ public class ConnectGameUI {
     private static final int BLANK = GameBoard.BLANK;
     private static final int RED = GameBoard.RED;
     private static final int YELLOW = GameBoard.YELLOW;
+
+    private int defaultSpaceSize = 50;
     
     private ConnectGame game;
     private int playerDisk;
     private GameMode currentMode;
     
     /**
-     * Constructs a new ConnectGameUI object with a new Connect4 game.
+     * Constructs a new ConnectGameUI object with a new Connect4 game, and the mode player v player.
      */
-    public ConnectGameUI(GameMode mode) {
+    public ConnectGameUI() {
+        this.game = new Connect4();
+        this.currentMode = GameMode.PLAYER_V_PLAYER;
+    }
+
+    /**
+     * Constructs a new ConnectGameUI object with a new Connect4 game.
+     * @param mode  The game mode for this ui
+     * @param playerDisk  The disk for the player to start with. If the mode is player v player, this paramter
+     * is ignored, so use the constructor ConnectGameUI().
+     */
+    public ConnectGameUI(GameMode mode, int playerDisk) {
         this.game = new Connect4();
         this.currentMode = mode;
+        this.playerDisk = playerDisk;
+        if (!isPlayersTurn()) {
+            Thread computerMove = new ComputerMove();
+            computerMove.start();
+        }
     }
 
     /**
      * Constructs a new ConnectGameUI object with the given ConnectGame and mode.
      * @param game  The game to use.
+     * @param mode  The game mode for this ui.
+     * @param playerDisk  The disk that the player will use. 
      */
-    public ConnectGameUI(ConnectGame game, GameMode mode) {
+    public ConnectGameUI(ConnectGame game, GameMode mode, int playerDisk) {
         this.game = game;
         this.currentMode = mode;
+        this.playerDisk = playerDisk;
+        if (!isPlayersTurn()) {
+            Thread computerMove = new ComputerMove();
+            computerMove.start();
+        }
     }
 
     /**
@@ -57,7 +85,7 @@ public class ConnectGameUI {
 
     /**
      * Returns the current game (ConnectGame Object).
-     * Note that this might not necessarily be thread safe. 
+     * Note that mutating this might not necessarily be thread safe. 
      */
     public ConnectGame getGame() {
         return game;
@@ -65,7 +93,7 @@ public class ConnectGameUI {
 
     /**
      * Returns the GameBoard of the current ConnectGame.
-     * Note that this might not necessarily be thread safe. 
+     * Note that mutating this might not necessarily be thread safe. 
      */
     public GameBoard getGameBoard() {
         return game.getGameBoard();
@@ -86,11 +114,57 @@ public class ConnectGameUI {
     }
 
     /**
+     * Use this method to set the size of the disks (Spaces) on the gameboard. 
+     * This property is by default set to 50.
+     * @param spaceSize  The new space size. Must be higher than 0.
+     * @throws IllegalArgumentException  if spaceSize <= 0.
+     */
+    public void setDefaultSpaceSize(int spaceSize) {
+        if (spaceSize > 0) {
+            defaultSpaceSize = spaceSize;
+        } else {
+            throw new IllegalArgumentException("The space size property of a ConnectGameUI must be greater than 0.");
+        }        
+    }
+    
+    /**
+     * Plays a random or computer move based on the current mode. 
+     */
+    public void playAuto() {
+        if (game.getWinner() == 3) {
+            return;
+        }
+        if (currentMode == GameMode.PLAYER_V_RANDOM) {
+            game.playRandom();
+        } else if (currentMode == GameMode.PLAYER_V_COMPUTER) {
+            game.playComputer();
+        }
+    }
+    
+    /**
      * This is an event handler for a MouseEvent from a GUI using this ConnectGameUI. 
-     * It should be run in a separate thread. 
+     * It should preferably be run in a separate thread. 
      * @param mouseEvent
      */
     public void mousePressed(MouseEvent mouseEvent) {
-        //TODO implement
+        if (isPlayersTurn()) { // If it is the player's turn, play their move.
+            final int playColumn = mouseEvent.getX() / defaultSpaceSize;
+            if (playColumn < gameColumns()) {
+                game.safePlay(playColumn);
+            }
+        }
+        if (currentMode != GameMode.PLAYER_V_PLAYER && !isPlayersTurn()) { // If it is now the computer's turn, play their move. 
+            playAuto();
+        }
+    }
+
+    /**
+     * This is just so the computer move can be played at the start of the game in another thread. 
+     */
+    private class ComputerMove extends Thread {
+        @Override 
+        public void run() {
+            playAuto();
+        }
     }
 }
--- src/connectgame/GUI.java
+++ src/connectgame/GUI.java
@@ -6,12 +6,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.BorderLayout;
 
 import javax.swing.*;
 
+
+//TODO make the game stop when someone's won.
 public class GUI extends MouseAdapter {
 
     private static final int DISK_SIZE = 50;
+    private static final ImageIcon[] DISK_ICONS = {
+        new ImageIcon("images/Blank.png"),
+        new ImageIcon("images/Red.png"),
+        new ImageIcon("images/Yellow.png")
+    };
 
     private ConnectGameUI ui;
     private Click currentClickThread;
@@ -19,29 +27,87 @@ public class GUI extends MouseAdapter {
     //Components
     private JFrame frame;
     private JPanel mainPanel;
+    private JLabel title;
+    private JLabel[][] board;
     
-    public GUI() {
-        frame = new JFrame();
-    }
-
     /**
-     * Initializes the GUI with all settings default:
+     * Creates a ConnectGame GUI with all settings default:
      * <ul>
      * <li>ConnectGame: Connect4
      * <li>Game Mode: Player v Player
-     * <li>
      */
-    public void init() {
-        ui = new ConnectGameUI(ConnectGameUI.GameMode.PLAYER_V_PLAYER);
-        frame.setSize(ui.gameColumns() * DISK_SIZE + 200, ui.gameRows() * DISK_SIZE + 100);
-        frame.setTitle("Connect 4");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.addMouseListener(this);
-        frame.setVisible(true); //TODO working on this currently
+    public GUI() {
+        frame = new JFrame();
+        ui = new ConnectGameUI(ConnectGameUI.GameMode.PLAYER_V_RANDOM, GameBoard.YELLOW);
+        init();
     }
 
+    /**
+     * Initializes the GUI.
+     */
+    private void init() {
+        final int width = (ui.gameColumns() + 6) * DISK_SIZE;
+        final int height = (ui.gameColumns() + 3) * DISK_SIZE;
+
+        mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(width, height, width, height));
+        mainPanel.setLayout(null);
+
+        title = new JLabel();
+        title.setText("<html><h1>Java Connect 4");
+        title.setBounds(10,5,200,30);
+
+        mainPanel.add(title);
+        
+        initBoard();
+        updateBoard();
+
+        frame.setSize(width, height);
+        frame.setTitle("Java Connect 4");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(mainPanel, BorderLayout.CENTER);
+    }
+
+    /**
+     * Initialize the board as an array of JLabels with ImageIcons, and add them to the mainPanel.
+     */
+    private void initBoard() {
+        final int columns = ui.gameColumns();
+        final int rows = ui.gameRows();
+        board = new JLabel[ui.gameColumns()][ui.gameRows()];
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                board[i][j] = new JLabel();
+                board[i][j].setBounds(i * DISK_SIZE, (rows - 1) * DISK_SIZE - (j - 1) * DISK_SIZE, DISK_SIZE, DISK_SIZE);
+                mainPanel.add(board[i][j]);
+            }
+        }
+    }
+
+    /**
+     * Updates the board to reflect the current GameBoard.
+     */
+    private void updateBoard() {
+        for (int i = 0; i < ui.gameColumns(); i++) {
+            for (int j = 0; j < ui.gameRows(); j++) {
+                board[i][j].setIcon(DISK_ICONS[ui.getGameBoard().get(i, j)]);
+            }
+        }
+    }
+
+    /**
+     * Show and start the GUI.
+     */
+    public void start() {
+        frame.addMouseListener(this);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Updates the GUI.
+     */
     public void updateGUI() {
-        // TODO: will implement this when I've created the GUI!!
+        updateBoard();
     }
 
     @Override
